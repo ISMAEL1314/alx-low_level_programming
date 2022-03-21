5873df00890402324018981e040c686f20382c61
ISMAEL1314 <ismaelhassane86@gmail.com>
2022-03-21 23:21:55 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,20 @@
-#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
 
 /**
-  * puts2 - Prints every other character of a string
-  * @str: String to be treated
-  *
-  * Return: void
-  */
-
+ * puts2 - prints every other character of a string
+ * @str: variable of string
+ * Returns: void
+ */
 void puts2(char *str)
 {
-	int i = 0;
-
-	while {str[i] != '\n')
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
+int c;
+for (c = 0; str[c] != 0; c++)
+{
+if (c % 2 != 1)
+{
+_putchar(str[c]);
+}
+}
+_putchar('\n');
 }
